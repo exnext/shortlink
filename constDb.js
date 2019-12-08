2bab69dfd5ab87f9126f0a52f8eb985645e38391
Krystian Prus <lamerhiob@poczta.onet.pl>
2019-12-08 22:15:55 +0100

    implementation

--- constDb.js
+++ constDb.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
 const FILE = `./shortlink.db`;
 
 const CREATE_TABLE = `CREATE TABLE IF NOT EXISTS shortlink (
@@ -11,15 +14,125 @@ const CREATE_TABLE = `CREATE TABLE IF NOT EXISTS shortlink (
     added DATE
 )`;
 
-// const INSERT = 'insert into LINKS values (?, ?)';
 const INSERT = `INSERT INTO shortlink VALUES ($tag, $url, $name, $description, $expire, $added)`;
 const SELECT = `SELECT * FROM shortlink WHERE tag=$tag LIMIT 1`;
 const SELECT_ALL = `SELECT * FROM shortlink`;
 
+fs.closeSync(fs.openSync(FILE, 'w'));
+
+let db;
+
+function init() {
+    return new Promise((resolve, reject) => {
+        try {
+            db = new sqlite3.Database(FILE, sqlite3.OPEN_READWRITE, (error) => {
+                if (error) {
+                    reject(error);
+                } {
+                    db.serialize(() => {
+                        db.run(CREATE_TABLE);
+                        resolve();
+                    });
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function insert(data) {
+    return new Promise((resolve, reject) => {
+        try {
+            let stmt = db.prepare(INSERT, data, (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    stmt.run()
+                        .finalize(() => {
+                            resolve(data);
+                        });
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function getUrl(tag) {
+    return new Promise((resolve, reject) => {
+        try {
+            let params = {
+                $tag: tag
+            }
+
+            let stmt = db.prepare(SELECT, params, (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    let url = undefined;
+
+                    stmt.each((error, row) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            url = row.url;
+                        }
+                    }).finalize(() => {
+                        if (url) {
+                            resolve(url);
+                        } else {
+                            reject('tag is not supported');
+                        }
+                    });
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function getUrlsList() {
+    return new Promise((resolve, reject) => {
+        try {
+            let stmt = db.prepare(SELECT_ALL, (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    let rows = [];
+
+                    stmt.each((error, row) => {
+                        rows.push(row);
+                    }).finalize(() => {
+                        resolve(rows);
+                    });
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function existTag(tag) {
+    return false;
+}
+
 module.exports = {
-    FILE,
-    CREATE_TABLE,
-    INSERT,
-    SELECT,
-    SELECT_ALL
-};
+    init,
+    insert,
+    getUrl,
+    getUrlsList,
+    existTag
+}
+
+
+// module.exports = {
+//     FILE,
+//     CREATE_TABLE,
+//     INSERT,
+//     SELECT,
+//     SELECT_ALL
+// };
--- shortlink.js
+++ shortlink.js
@@ -1,94 +1,116 @@
 'use strict';
 
-const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
+// const fs = require('fs');
+// const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
-const app = express();
 
 const constDb = require('./constDb');
- 
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-fs.closeSync(fs.openSync(constDb.FILE, 'w'));
 
-let db = new sqlite3.Database(constDb.FILE, sqlite3.OPEN_READWRITE, (error) => {
-    if (error) {
-        console.error(error.message);
-        return;
-    }
- 
-    initDB();
- 
+constDb.init().then(() => {
     app.listen(PORT, HOST);
-    console.log(`Running on http://${HOST}:${PORT}`);
+    console.log(`Running on http://${HOST}:${PORT}`);    
 });
- 
-function initDB() {
-    db.serialize(() => {
-        db.run(constDb.CREATE_TABLE);
-    });
-}
- 
+
+// fs.closeSync(fs.openSync(constDb.FILE, 'w'));
+
+// let db = new sqlite3.Database(constDb.FILE, sqlite3.OPEN_READWRITE, (error) => {
+//     if (error) {
+//         console.error(error.message);
+//         return;
+//     }
+
+//     initDB();
+
+//     app.listen(PORT, HOST);
+//     console.log(`Running on http://${HOST}:${PORT}`);
+// });
+
+// function initDB() {
+//     db.serialize(() => {
+//         db.run(constDb.CREATE_TABLE);
+//     });
+// }
+
 app.get('/', (req, res) => {
     if (req.query.url) {
-        try {
-            let params = {
-                $tag: generateTag({}),
-                $url: req.query.url,
-                $name: null,
-                $description: null,
-                $expire: req.query.expire,
-                $added: new Date()
-            }       
-    
-            let stmt = db.prepare(constDb.INSERT, params, (error) => {
-                stmt.run().finalize(() => {
-                    res.send('added ' + params.$tag);
-                });
-            });  
-        } catch (error) {
+        let data = buildData(req.query);
+
+        constDb.insert(data)
+        .then((result) => {
+            res.send('added ' + result.$tag);
+        })
+        .catch((error) => {
             res.send(error);
-        }
-    } else if (req.query.list) {
-        let stmt = db.prepare(constDb.SELECT_ALL, (error) => {
-            let rows = [];
-            
-            stmt.each((error, row) => {
-                rows.push(row);
-            }).finalize(() => {
-                res.send(rows);
-            });
         });
+    } else if (req.query.list) {
+        constDb.getUrlsList()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((error) => {
+            res.send(error);
+        });
+        // let stmt = db.prepare(constDb.SELECT_ALL, (error) => {
+        //     let rows = [];
+
+        //     stmt.each((error, row) => {
+        //         rows.push(row);
+        //     }).finalize(() => {
+        //         res.send(rows);
+        //     });
+        // });
     } else {
         res.send('parameter url is not found\n');
     }
 });
 
 app.post('/', (req, res) => {
+    let data = buildData(req.body);
 
+    constDb.insert(data)
+    .then((result) => {
+        res.send('added ' + result.$tag);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
 });
 
 app.get('/:tag', (req, res) => {
-    db.serialize(() => {
-        let params = {
-            $tag: req.params.tag
-        }
- 
-        let stmt = db.prepare(constDb.SELECT, params, (error) => {
-            let url = undefined;
-            
-            stmt.each((error, row) => {
-                url = row.url;
-            }).finalize(() => {
-                if (url) {
-                    res.redirect(url);
-                } else {
-                    res.send('tag is not supported');
-                }
-            });
-        });
+    constDb.getUrl(req.params.tag)
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((error) => {
+        res.send(error);
     });
+    // db.serialize(() => {
+    //     let params = {
+    //         $tag: req.params.tag
+    //     }
+
+    //     let stmt = db.prepare(constDb.SELECT, params, (error) => {
+    //         let url = undefined;
+
+    //         stmt.each((error, row) => {
+    //             url = row.url;
+    //         }).finalize(() => {
+    //             if (url) {
+    //                 res.redirect(url);
+    //             } else {
+    //                 res.send('tag is not supported');
+    //             }
+    //         });
+    //     });
+    // });
 });
 
 function generateTag(params, attempts = 10) {
@@ -101,7 +123,7 @@ function generateTag(params, attempts = 10) {
 
     tag = tag || Math.random().toString(36).substring(2, 15);
 
-    if (existTag(tag)) {
+    if (constDb.existTag(tag)) {
         if (allowUserTag) {
             throw 'the tag is used';
         } else if (attempts) {
@@ -110,10 +132,40 @@ function generateTag(params, attempts = 10) {
             throw 'internal error, please try again';
         }
     }
-    
+
     return tag;
 }
 
-function existTag(tag) {
-    return false;
+// function existTag(tag) {
+//     return false;
+// }
+
+function buildData(data) {
+    return {
+        $tag: generateTag(data),
+        $url: data.url,
+        $name: data.name,
+        $description: data.description,
+        $expire: data.expire,
+        $added: new Date()
+    }
 }
+
+// function insert(data) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             let stmt = db.prepare(constDb.INSERT, data, (error) => {
+//                 if (error) {
+//                     reject(error);
+//                 } else {
+//                     stmt.run()
+//                     .finalize(() => {
+//                         resolve(data);
+//                     });
+//                 }
+//             });
+//         } catch (error) {
+//             reject(error);
+//         }
+//     });
+// }
